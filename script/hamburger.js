9fce9f291f3a1177fc71beea26a429154499d723
SputnikTH <rachata9866@gmail.com>
2025-03-29 23:33:41 -0700

    fixed the overlay

--- script/hamburger.js
+++ script/hamburger.js
@@ -1,30 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const nav = document.querySelector('.nav');
-    const header = document.querySelector('.header');
+  const nav = document.querySelector('.nav');
+  const header = document.querySelector('.header');
+  const overlay = document.querySelector('.overlay');
+  // Create menu button
+  const menuButton = document.createElement('button');
+  menuButton.className = 'menu-button';
+
+  const icon = document.createElement('img');
+  icon.src = './assets/images/icon-menu.svg';
+  icon.alt = 'Menu';
+  icon.className = 'menu-icon';
+
+  menuButton.appendChild(icon);
+  header.appendChild(menuButton);
+
+  // Toggle navigation
+  menuButton.addEventListener('click', () => {
+    const isOpen = nav.classList.toggle('nav-open');
   
-    // Create menu button
-    const menuButton = document.createElement('button');
-    menuButton.className = 'menu-button';
+    document.body.classList.toggle('menu-open', isOpen);
   
-    const icon = document.createElement('img');
+    if (isOpen) {
+      icon.src = './assets/images/icon-menu-close.svg';
+      icon.alt = 'Close Menu';
+    } else {
+      icon.src = './assets/images/icon-menu.svg';
+      icon.alt = 'Menu';
+    }
+  });
+
+  overlay.addEventListener('click', () => {
+    nav.classList.remove('nav-open');
+    document.body.classList.remove('menu-open');
     icon.src = './assets/images/icon-menu.svg';
     icon.alt = 'Menu';
-    icon.className = 'menu-icon';
-  
-    menuButton.appendChild(icon);
-    header.appendChild(menuButton);
-  
-    // Toggle navigation
-    menuButton.addEventListener('click', () => {
-      const isOpen = nav.classList.toggle('nav-open'); // toggle and get state
-  
-      if (isOpen) {
-        icon.src = './assets/images/icon-menu-close.svg';
-        icon.alt = 'Close Menu';
-      } else {
-        icon.src = './assets/images/icon-menu.svg';
-        icon.alt = 'Menu';
-      }
-    });
   });
-  
+});
